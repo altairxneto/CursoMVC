587dcf8402b05cab33ad25cd83794720f454e74b
Altair Neto <altairxneto@gmail.com>
2023-05-23 21:33:48 -0300

    Criei a classe AppDbContext

---
+++ ProjetoLanchesMVC/ProjetoLanchesMVC/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoLanchesMVC.Models;
+
+namespace ProjetoLanchesMVC.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Lanche> Lanche { get; set; }
+
+    }
+}
