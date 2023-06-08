eb8dd97908bc39b5c67acc375faefe5dad5b0823
Altair Neto <altairxneto@gmail.com>
2023-06-08 15:11:51 -0300

    Revert "Revert "Implementei minha primeira migração""
    This reverts commit 570a92b95ea477f9c6c46114fc8ca58fd3c2bcf7.

--- ProjetoLanchesMVC/ProjetoLanchesMVC/Models/Categoria.cs
+++ ProjetoLanchesMVC/ProjetoLanchesMVC/Models/Categoria.cs
@@ -8,13 +8,13 @@ namespace ProjetoLanchesMVC.Models
     {
         [Key]
         public int CategoriaId { get; set; }
-        [StringLength(100, ErrorMessage = "O tamanho máximo é 100 caracteres")]
-        [Required(ErrorMessage = "Informe o nome da categoria")]
+        [StringLength(100, ErrorMessage ="O tamanho máximo é 100 caracteres")]
+        [Required(ErrorMessage ="Informe o nome da categoria")]
         [Display(Name = "Nome")]
         public string CategoriaNome { get; set; }
-        [StringLength(200, ErrorMessage = "O tamanho máximo é 200 caracteres")]
-        [Required(ErrorMessage = "Informe a descrição da categoria")]
-        [Display(Name = "Nome")]
+        [StringLength(200, ErrorMessage ="O tamanho máximo é 200 caracteres")]
+        [Required(ErrorMessage ="Informe a descrição da categoria")]
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
         public List<Lanche> Lanches { get; set;}
     }
