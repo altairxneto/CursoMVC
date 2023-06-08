570a92b95ea477f9c6c46114fc8ca58fd3c2bcf7
Altair Neto <altairxneto@gmail.com>
2023-06-08 15:10:34 -0300

    Revert "Implementei minha primeira migração"
    This reverts commit 0edfb0de47960b73679478704ad79315d73e60c0.

--- ProjetoLanchesMVC/ProjetoLanchesMVC/Models/ErrorViewModel.cs
+++ ProjetoLanchesMVC/ProjetoLanchesMVC/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace ProjetoLanchesMVC.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
