70880b20d46241534b542a526f585a89d16b2342
Altair Neto <altairxneto@gmail.com>
2023-06-13 16:33:17 -0300

    Implementei a interface ILancheRepository

---
+++ ProjetoLanchesMVC/ProjetoLanchesMVC/Repositories/Interfaces/ILancheRepository.cs
@@ -0,0 +1,11 @@
+﻿using ProjetoLanchesMVC.Models;
+
+namespace ProjetoLanchesMVC.Repositories.Interfaces
+{
+    public interface ILancheRepository
+    {
+        IEnumerable<Lanche> Lanches { get; }
+        IEnumerable<Lanche> LanchesPreferidos { get; }
+        Lanche GetLancheById(int lancheId);
+    }
+}
