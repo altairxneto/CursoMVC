76c14421463044ad4e4c7c66ce5f8842fb50d84d
Altair Neto <altairxneto@gmail.com>
2023-06-16 21:50:20 -0300

    Exclui o IActionResult Demo

--- ProjetoLanchesMVC/ProjetoLanchesMVC/Controllers/HomeController.cs
+++ ProjetoLanchesMVC/ProjetoLanchesMVC/Controllers/HomeController.cs
@@ -12,10 +12,6 @@ namespace ProjetoLanchesMVC.Controllers
             return View();
         }
 
-        public IActionResult Demo()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
