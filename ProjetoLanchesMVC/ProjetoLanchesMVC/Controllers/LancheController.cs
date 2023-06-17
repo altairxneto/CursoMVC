f3387aeedafb051b29c10f5f79d508883e06a08e
Altair Neto <altairxneto@gmail.com>
2023-06-16 22:20:28 -0300

    Implementa o Controller Lanche

---
+++ ProjetoLanchesMVC/ProjetoLanchesMVC/Controllers/LancheController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoLanchesMVC.Repositories.Interfaces;
+
+namespace ProjetoLanchesMVC.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
