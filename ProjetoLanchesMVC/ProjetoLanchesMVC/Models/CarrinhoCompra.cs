f573805db01a42a2f7b45a43bbbd39ae5d2d170c
Altair Neto <altairxneto@gmail.com>
2023-06-20 17:04:03 -0300

    Implementa a classe carrinho de compras

---
+++ ProjetoLanchesMVC/ProjetoLanchesMVC/Models/CarrinhoCompra.cs
@@ -0,0 +1,38 @@
+﻿using ProjetoLanchesMVC.Context;
+
+namespace ProjetoLanchesMVC.Models
+{
+    public class CarrinhoCompra
+    {
+        private readonly AppDbContext _context;
+
+        public CarrinhoCompra(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        public string CarrinhoCompraId { get; set; }
+        public List<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
+        public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+        {
+            //Define uma sessão
+            ISession session =
+                services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            //Obtem um serviço do tipo do nosso contexto
+            var context = services.GetService<AppDbContext>();
+
+            //Obtem ou gera o Id do carrinho
+            string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+            //Atribui o Id do carrinho na Sessão
+            session.SetString("CarrinhoId", carrinhoId);
+
+            //Retorna o carrinho com o contexto e o Id atribuido ou obtido
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId,
+            };
+        }
+    }
+}
