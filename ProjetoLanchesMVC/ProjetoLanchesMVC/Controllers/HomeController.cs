b769bed9143ec04d08e6dd48f96d405c9488e185
Altair Neto <altairxneto@gmail.com>
2023-07-12 19:34:04 -0300

    Implementa o controller Home para mostrar os lanches preferidos

--- ProjetoLanchesMVC/ProjetoLanchesMVC/Controllers/HomeController.cs
+++ ProjetoLanchesMVC/ProjetoLanchesMVC/Controllers/HomeController.cs
@@ -1,15 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProjetoLanchesMVC.Models;
+using ProjetoLanchesMVC.Repositories.Interfaces;
+using ProjetoLanchesMVC.ViewModels;
 using System.Diagnostics;
 
 namespace ProjetoLanchesMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILancheRepository _lancheRepository;
+
+        public HomeController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
 
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                LanchesPreferidos = _lancheRepository.LanchesPreferidos
+            };
+            return View(homeViewModel);
         }
 
 
---
+++ ProjetoLanchesMVC/ProjetoLanchesMVC/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using ProjetoLanchesMVC.Models;
+
+namespace ProjetoLanchesMVC.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Lanche> LanchesPreferidos { get; set; }
+    }
+}
