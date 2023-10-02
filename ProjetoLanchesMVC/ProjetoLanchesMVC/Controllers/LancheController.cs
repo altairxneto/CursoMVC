21b6cfaa1a734fcd5737b79984291a0fa34da0ce
Altair Neto <altairxneto@gmail.com>
2023-10-02 20:33:15 -0300

    Otimiza a implementação do Lanche Controller no método List

--- ProjetoLanchesMVC/ProjetoLanchesMVC/Controllers/LancheController.cs
+++ ProjetoLanchesMVC/ProjetoLanchesMVC/Controllers/LancheController.cs
@@ -25,18 +25,21 @@ namespace ProjetoLanchesMVC.Controllers
             }
             else
             {
-                if(string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase))
-                {
-                    lanches = _lancheRepository.Lanches
-                        .Where(l => l.Categoria.CategoriaNome.Equals("Normal"))
-                        .OrderBy(l => l.Nome);
-                }
-                else
-                {
-                    lanches = _lancheRepository.Lanches
-                        .Where(l => l.Categoria.CategoriaNome.Equals("Natural"))
-                        .OrderBy(l => l.Nome);
-                }
+                //if(string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase))
+                //{
+                //    lanches = _lancheRepository.Lanches
+                //        .Where(l => l.Categoria.CategoriaNome.Equals("Normal"))
+                //        .OrderBy(l => l.Nome);
+                //}
+                //else
+                //{
+                //    lanches = _lancheRepository.Lanches
+                //        .Where(l => l.Categoria.CategoriaNome.Equals("Natural"))
+                //        .OrderBy(l => l.Nome);
+                //}
+                lanches = _lancheRepository.Lanches
+                    .Where(l => l.Categoria.CategoriaNome.Equals(categoria))
+                    .OrderBy(l => l.Nome);
                 categoriaAtual = categoria;
             }
 
