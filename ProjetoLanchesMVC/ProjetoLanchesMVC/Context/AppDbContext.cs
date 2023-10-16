780e096a2565b748949c8eb1736a4035be874c14
Altair Neto <altairxneto@gmail.com>
2023-10-16 17:30:24 -0300

    Implementa a criação das tabelas das models Pedido e PedidoDetalhe

--- ProjetoLanchesMVC/ProjetoLanchesMVC/Context/AppDbContext.cs
+++ ProjetoLanchesMVC/ProjetoLanchesMVC/Context/AppDbContext.cs
@@ -10,6 +10,8 @@ namespace ProjetoLanchesMVC.Context
         public DbSet<Categoria> Categoria { get; set; }
         public DbSet<Lanche> Lanche { get; set; }
         public DbSet<CarrinhoCompraItem> CarrinhoCompraItem { get; set; }
+        public DbSet<Pedido> Pedido { get; set; }
+        public DbSet<PedidoDetalhe> PedidoDetalhe { get; set; }
 
     }
 }
